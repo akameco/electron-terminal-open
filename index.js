064db4a3420597dab6114147dd5428f3b0115412
akameco <akameco.t@gmail.com>
2019-09-20 04:02:50 +0900

    refactor: use async/await insted of co

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const path = require('path')
 const fs = require('fs')
 const pify = require('pify')
 const ejs = require('ejs')
-const co = require('co')
 const commandInstaller = require('command-installer')
 const fsP = pify(fs)
 
@@ -11,26 +10,16 @@ function getResourcesPath() {
   return process.resourcesPath
 }
 
-module.exports = (app, opt) => {
-  return new Promise((resolve, reject) => {
-    if (!app) {
-      reject(new Error('Expected a electron.app'))
-    }
-    co(function*() {
-      const name = app.getName()
-      const from = path.resolve(__dirname, 'open.sh.ejs')
-      const defaultOpt = { name, bundleId: `com.electron.${name}` }
-      const output = yield pify(ejs.renderFile)(
-        from,
-        Object.assign({}, defaultOpt, opt)
-      )
-      const outputPath = path.resolve(getResourcesPath(), `${name}.sh`)
-      yield fsP.writeFile(outputPath, output)
-      yield fsP.chmod(outputPath, '755')
-      yield commandInstaller(outputPath, name)
-      resolve()
-    }).catch(err => {
-      reject(err)
-    })
-  })
+module.exports = async (app, opt) => {
+  if (!app) {
+    new Error('Expected a electron.app')
+  }
+  const name = app.getName()
+  const from = path.resolve(__dirname, 'open.sh.ejs')
+  const defaultOpt = { name, bundleId: `com.electron.${name}` }
+  const output = await pify(ejs.renderFile)(from, { ...defaultOpt, ...opt })
+  const outputPath = path.resolve(getResourcesPath(), `${name}.sh`)
+  await fsP.writeFile(outputPath, output)
+  await fsP.chmod(outputPath, '755')
+  await commandInstaller(outputPath, name)
 }
